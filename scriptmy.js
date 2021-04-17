1452895b244eb0124ed24c982e8fd5f2b86a6e58
NikitoS549 <81013986+NikitoS549@users.noreply.github.com>
2021-04-17 23:16:08 +0300

    Add files via upload

--- scriptmy.js
+++ scriptmy.js
@@ -18,11 +18,23 @@ function Srav()
     d=document
     var a = Number(d.form2.x1.value)//для поля ввода a
     var b = Number(d.form2.x2.value)//для поля ввода b
-    var srav = eval(Math.max(a,b))//Выбор максимального значения
-    var y2 = Number(d.form2.y2.value)//для поля вывода y2
-    if(a!=0 && b!=0)
-    {
-    y2 = eval(srav);
-    }
+    var y2 = Math.max(a,b)//Выбор максимального значения
     d.form2.y2.value=y2;
+}
+
+function Imya()
+{
+d=document
+var admin, name; // можно объявить две переменные через запятую
+name = "Никитос";
+admin = name;
+alert( admin ); // "Никита"
+}
+
+function Old()
+{
+d-document
+var age = Number(d.form4.x1.value)
+age = old;
+alert( old );
 }
