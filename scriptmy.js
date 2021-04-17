d9fd207c679734af2a7a216265d5be42367bb58c
NikitoS549 <81013986+NikitoS549@users.noreply.github.com>
2021-04-17 16:21:42 +0300

    Add files via upload

---
+++ scriptmy.js
@@ -0,0 +1,28 @@
+function kvadrt()
+{
+d=document
+var a = Number(d.form1.num1.value)//для поля ввода a
+var b = Number(d.form1.num2.value)//для поля ввода b
+var c = Number(d.form1.num3.value)//для поля ввода c
+var diz = eval(Math.pow(b,2)-4*a*c)//расчёт дискримина́нта
+var x1 = Number(d.form1.x1.value)//для поля вывода х1
+if(a!=0 && b!=0 && c!=0)
+{
+x1 = eval(diz);
+}
+d.form1.x1.value=x1;
+}
+
+function Srav()
+{
+    d=document
+    var a = Number(d.form2.x1.value)//для поля ввода a
+    var b = Number(d.form2.x2.value)//для поля ввода b
+    var srav = eval(Math.max(a,b))//Выбор максимального значения
+    var y2 = Number(d.form2.y2.value)//для поля вывода y2
+    if(a!=0 && b!=0)
+    {
+    y2 = eval(srav);
+    }
+    d.form2.y2.value=y2;
+}
